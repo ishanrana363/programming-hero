538b67a9b47bbbcbb3a20137713ae29a1acec251
ishanrana363 <ishanrana363@gmail.com>
2024-09-02 22:07:58 +0600

    simple mathematic operation

---
+++ milestone-4-hello-javaScript/module-16-introduction-javaScript/mathmatical-operation/main.js
@@ -0,0 +1,22 @@
+var price1 = 30;
+var price2 = 52
+var totalPrice = price1+price2;
+
+console.log(totalPrice);
+
+var totalPrice = price1-price2;
+
+console.log(totalPrice);
+
+
+var orangePrice = 30;
+
+var quantity = 10;
+
+var totalPrice = orangePrice * quantity;
+
+console.log(totalPrice);
+
+var money = 300;
+var getMoney = 300/10;
+console.log(getMoney);
