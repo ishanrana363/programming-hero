40563cf28fd30b549a5aaa76b0c4151ec9385bd8
ishanrana363 <ishanrana363@gmail.com>
2024-09-02 22:59:08 +0600

    parseInt parseFloat

---
+++ milestone-4-hello-javaScript/module-16-introduction-javaScript/(advanced) String Concatenation, type conversion parseInt parseFloat/main.js
@@ -0,0 +1,15 @@
+var firstName = "Shubo";
+var lastName = "Hossain";
+const fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var a = "20";
+var aParseInt = parseInt(a)
+var b = 30;
+
+console.log(aParseInt+b);
+
+var gpa = "3.40"
+
+console.log(parseFloat(gpa)+3);
