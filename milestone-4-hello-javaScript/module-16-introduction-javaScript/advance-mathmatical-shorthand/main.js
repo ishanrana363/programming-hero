08853b9881adf609c8ad9ed2febc8a30d20b1ae8
ishanrana363 <ishanrana363@gmail.com>
2024-09-02 22:45:59 +0600

    javascript short hand property

--- milestone-4-hello-javaScript/module-16-introduction-javaScript/advance-mathmatical-shorthand/main.js
+++ milestone-4-hello-javaScript/module-16-introduction-javaScript/advance-mathmatical-shorthand/main.js
@@ -2,4 +2,22 @@ var price = 30
 
 price+=30
 
+console.log(price);
+
+price*=10;
+
+console.log(price)
+
+price-=20;
+
+console.log(price);
+
+price/=40;
+console.log(price);
+
+price++;
+console.log(price);
+
+
+--price;
 console.log(price)
