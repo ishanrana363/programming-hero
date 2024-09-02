498398ae681ba2a8cb68b7d975f229c23c6d4d3f
ishanrana363 <ishanrana363@gmail.com>
2024-09-02 21:59:39 +0600

    variabe name convension

---
+++ milestone-4-hello-javaScript/module-16-introduction-javaScript/variabe-naming-convention/main.js
@@ -0,0 +1,21 @@
+// // naming convention
+
+// var first name = "ishanrana" // wrong
+
+// var firstName = "Ishan rana" // right camel-case
+
+// var total price = 300 // wrong
+
+// var total_price = 300 // write snake-case
+
+// var Home Distice = "Dinajpur";  // wrong
+
+// var HomeDistice = "Dinajpur" // write pascal case
+
+// var 123Age = 21 //wrong
+
+// var age123 = 21;
+
+var age$ = 21;
+
+console.log(age$)
