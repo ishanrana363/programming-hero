b7c09fd7d2e0fe1830cfc273ef8f5b0a628548ff
ishanrana363 <ishanrana363@gmail.com>
2024-09-02 23:50:57 +0600

    task 2 completed

---
+++ milestone-4-hello-javaScript/module-16-introduction-javaScript/task/task2.js
@@ -0,0 +1,9 @@
+var bangle = 70;
+var math = 80;
+var chemistry = 90;
+var biology = 89;
+
+var result = bangle+math+chemistry+bangle
+var totalResult = result/4
+var finalResult = parseFloat(totalResult);
+console.log( typeof finalResult)
