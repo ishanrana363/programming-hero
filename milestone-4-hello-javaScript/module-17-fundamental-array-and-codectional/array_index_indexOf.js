a0a5ea07e708e679fe16d6ce868eeefb24bc9df5
ishanrana363 <ishanrana363@gmail.com>
2024-09-03 21:50:54 +0600

    array_index_indexOf

---
+++ milestone-4-hello-javaScript/module-17-fundamental-array-and-codectional/array_index_indexOf.js
@@ -0,0 +1,10 @@
+var number = [2, 3, 4, 5, 6, 7];
+
+console.log(number[0]); // Outputs 2
+
+number[0] = "ishan"; 
+number[0] = 100;
+console.log(number); 
+
+var indexLength = number.indexOf(3); // Find the index of the value 6
+console.log(indexLength); // Outputs 4 (since 6 is at index 4)
