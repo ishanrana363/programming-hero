3b46707f8e7e71ed9b6d9b215a988e0321e7d5db
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 22:59:10 +0600

    problem 2 done

--- milestone-4-hello-javaScript/module-18/problem.js
+++ milestone-4-hello-javaScript/module-18/problem.js
@@ -1,3 +1,7 @@
-for(let i = 0 ; i<=39;i++ ){
-    console.log(i,"ajke amar mon valo nai ");
+// for(let i = 0 ; i<=39;i++ ){
+//     console.log(i,"ajke amar mon valo nai ");
+// }
+
+for(let i =58; i<=98;i++){
+    console.log(i);
 }
