5851997751609cc5601212f80649e697ca04df5b
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 22:26:50 +0600

    for loop reverse

--- milestone-4-hello-javaScript/module-18/reverse.js
+++ milestone-4-hello-javaScript/module-18/reverse.js
@@ -1,6 +1,12 @@
-let number = 10;
+// let number = 10;
 
-while(number>0){
-    console.log(number);
-    number--;
+// while(number>0){
+//     console.log(number);
+//     number--;
+// }
+
+let number = 0;
+
+for(var i =10;i>=1;i-- ){
+    console.log(i);
 }
