22e189d9327dc4b77f993eb9d589bbae60f37f45
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 22:14:16 +0600

    break continue

---
+++ milestone-4-hello-javaScript/module-18/break-loop.js
@@ -0,0 +1,8 @@
+const names = ['ishan','rana','meherul','islam','rahman'];
+for(let i = 0; i<names.length;i++){
+    let name = names[i];
+    if(name=="islam"){
+        break;
+    }
+    console.log(name);
+};
