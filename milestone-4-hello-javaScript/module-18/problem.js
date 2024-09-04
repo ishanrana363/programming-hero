9c2b637f667912bff88cc87504960e1731fca77e
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 23:07:49 +0600

    5 problem done

--- milestone-4-hello-javaScript/module-18/problem.js
+++ milestone-4-hello-javaScript/module-18/problem.js
@@ -9,6 +9,9 @@
 // for(let i = 412; i<=456;i=i+2){
 //     console.log(i);
 // }
-for( let i= 581; i<=623;i=i+2 ){
-    console.log(i);
-}
+// for( let i= 581; i<=623;i=i+2 ){
+//     console.log(i);
+// }
+
+// const array = ["html","css","tailwind","daisyUi","javaScript"];
+// console.log(array);
