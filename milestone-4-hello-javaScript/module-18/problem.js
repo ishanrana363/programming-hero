9a5f0b0dea5e39fdde2737b6af396e0b6305f92c
ishanrana363 <ishanrana363@gmail.com>
2024-09-04 23:03:31 +0600

    4 problem done

--- milestone-4-hello-javaScript/module-18/problem.js
+++ milestone-4-hello-javaScript/module-18/problem.js
@@ -6,6 +6,9 @@
 //     console.log(i);
 // }
 
-for(let i = 412; i<=456;i=i+2){
+// for(let i = 412; i<=456;i=i+2){
+//     console.log(i);
+// }
+for( let i= 581; i<=623;i=i+2 ){
     console.log(i);
 }
