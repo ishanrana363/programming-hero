22c18623c47f9e7756f62a4aaf5fb6ebeb26d624
ishanrana363 <ishanrana363@gmail.com>
2024-09-07 23:05:09 +0600

    slice

---
+++ milestone-4-hello-javaScript/module-19/slice.js
@@ -0,0 +1,22 @@
+// const data = "dinajpure";
+// console.log(data);
+// // const sliceData = data.slice(0,6);
+// // console.log(sliceData);
+// console.log(data.slice(0,5));
+
+
+// const datas = "i am";
+
+// const data = datas.split(' ');
+// console.log(data[0])
+// console.log(data)
+
+const friends = 'rahim, karim, jodu, modu, kodu';
+console.log(friends);
+console.log( friends.split(', ')[1] );
+console.log(friends);
+
+const firstName = "ishan";
+const lastName = "rana";
+const fullName = firstName.concat(" ").concat(lastName);
+console.log(fullName);
