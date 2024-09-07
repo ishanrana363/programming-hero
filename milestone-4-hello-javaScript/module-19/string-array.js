4731f8707ca785cb94eb35e5a359d17cd9e6591a
ishanrana363 <ishanrana363@gmail.com>
2024-09-07 21:52:51 +0600

    array string

---
+++ milestone-4-hello-javaScript/module-19/string-array.js
@@ -0,0 +1,15 @@
+var data = new String("Ishan");
+console.log(typeof data );
+console.log(data);
+
+
+let dataList = [12,23,34,44,70];
+console.log(dataList);
+dataList[1] = 50;
+console.log(dataList);
+
+
+let name = 'console';
+console.log(name);
+name[0] = "d"
+console.log(name)
