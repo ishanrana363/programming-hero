3d52848f164f127502e01fe0b1040a60b735d829
ishanrana363 <ishanrana363@gmail.com>
2024-09-08 08:23:34 +0600

    even odd

--- milestone-4-hello-javaScript/module-20/isEven.js
+++ milestone-4-hello-javaScript/module-20/isEven.js
@@ -1,3 +1,13 @@
 function isEven (number) {
+    const reminder = number%2;
+    if(reminder===0){
+        // console.log(`${number} number is even`)
+        return true;
+    }else{
+        // console.log(`${number} is odd number `);
+        return false;
+    }
+};
 
-}
+const checkEvenNumber = 21;
+console.log(isEven(checkEvenNumber));
