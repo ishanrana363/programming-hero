5293f14f53a825fff4196facbc9f9a1819c7f64d
ishanrana363 <ishanrana363@gmail.com>
2024-09-08 08:18:37 +0600

    inche to feet

---
+++ milestone-4-hello-javaScript/module-20/inch-to-feet.js
@@ -0,0 +1,14 @@
+// const daduInch = 100;
+// const daduFeet = daduInch / 12;
+// console.log(`dadu feet is ${daduFeet.toFixed(2)}`);
+// const myInche = 144;
+// const myFeet = myInche/12;
+// console.log(`my feet is ${myFeet}`)
+
+function inchToFeet (inch) {
+    const feet = inch/12;
+    return feet;
+}
+
+const inches = 120;
+console.log(inchToFeet(inches));
