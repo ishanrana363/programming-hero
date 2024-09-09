37c8df3c931469ecdcf92f2ee66dd0cbf091bce3
ishanrana363 <ishanrana363@gmail.com>
2024-09-09 08:00:01 +0600

    array slice split

---
+++ milestone-4-hello-javaScript/module-21/split.js
@@ -0,0 +1,11 @@
+let lyrics = "tumi bondu kala pakhi. ami jon ki. bosonto kala tomar dka hoyni";
+
+let sliceData = lyrics.slice(0 , 12);
+
+// console.log(sliceData);
+
+// const sbuStrData = lyrics.substring(2,10);
+// console.log(sbuStrData);
+
+let splitData = lyrics.split(" ");
+console.log(splitData.join(","))
