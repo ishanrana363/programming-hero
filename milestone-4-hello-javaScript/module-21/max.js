42eb9adf22943d3536044ca1632fcb1714ede0de
ishanrana363 <ishanrana363@gmail.com>
2024-09-10 08:12:54 +0600

    max number

---
+++ milestone-4-hello-javaScript/module-21/max.js
@@ -0,0 +1,11 @@
+let a = 30;
+let b = 25;
+let c = 20;
+
+if(a>b&&a>c){
+    console.log(`the biggest number of a ${a}`)
+}else if (b>a&&b>c) {
+    console.log(`the biggest number b ${b}`)
+}else{
+    console.log(`the biggest number c ${c}`);
+}
