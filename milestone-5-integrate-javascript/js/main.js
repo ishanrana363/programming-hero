0cb23741b49d3575e28848f01a29b047fb80d835
ishanrana363 <ishanrana363@gmail.com>
2024-09-12 06:26:17 +0600

    update

---
+++ milestone-5-integrate-javascript/js/main.js
@@ -0,0 +1,38 @@
+// console.log("hello dom");
+// console.log(document.body);
+
+// let liTagName = document.getElementsByTagName("li");
+// for(let li of liTagName ){
+//     console.log(li.innerText)
+// }
+
+// let h1TagName = document.getElementsByTagName("h1");
+// for( let h1 of h1TagName ){
+//     console.log(h1);
+//     console.log(h1.innerText)
+// }
+
+// let name = document.getElementById("nam");
+
+// console.log(name);
+
+// let nameList = document.getElementsByClassName("name-list");
+// console.log(nameList);
+
+// for (let singleName of nameList) {
+//     singleName.style.backgroundColor = "red";
+//     singleName.style.padding = "20px";
+//     singleName.style.paddingBottom = "20px"; // Redundant, since padding already applies bottom
+//     singleName.style.border = "2px solid black";
+    
+//     // Correctly adding a class using classList
+//     singleName.classList.add("largeText"); // "largeText" should be a string
+// }
+
+// let sectionMain = document.querySelector("#sss ul ");
+// console.log(sectionMain);
+// let li = document.createElement("li");
+// li.innerText = "create new li";
+
+// sectionMain.append(li)
+    
