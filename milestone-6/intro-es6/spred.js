45931753ae46e9fdf24f2ce05e193ddffa3bde6d
ishanrana363 <ishanrana363@gmail.com>
2024-09-13 19:35:37 +0600

    spread operator

---
+++ milestone-6/intro-es6/spred.js
@@ -0,0 +1,12 @@
+// let num = Math.max(12,34,56,78,23,45,766);
+// console.log(num)
+
+// let arr = [12,20,10,34,556];
+// let maxNumber = Math.max(...arr);
+// console.log(maxNumber)
+
+const friends = ['jodu',"modue","kodu"];
+let bondus = [...friends] ;
+bondus.push("moyniul");
+console.log(friends)
+console.log(bondus);
