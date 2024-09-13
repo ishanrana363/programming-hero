c6259f13fde22daa44a4898aeed46e5f8b8f96d8
ishanrana363 <ishanrana363@gmail.com>
2024-09-13 14:49:47 +0600

    let var const

---
+++ milestone-6/intro-es6/let-vs-var-const.js
@@ -0,0 +1,30 @@
+// var not allow
+// let allow re-assign
+// not allow to re-assign
+
+
+// const number = 30;
+// number = 60
+
+// console.log(number)
+
+let number = 30;
+number = 50;
+console.log(number);
+
+// const arr = [23,45,10,34,50,23];
+// // arr = [33,44,64,12,43,24,56];
+// arr[2] = 3493
+// console.log(arr)
+
+const man = {
+    name :"Ishan",
+    age : 21
+};
+
+// man = {
+//     name : "rana"
+// }
+man.name = "rana"
+
+console.log(man)
