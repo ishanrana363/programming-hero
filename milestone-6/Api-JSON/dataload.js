c5fd9aa0beadce6f52555bdf04ecc94ee574eecc
ishanrana363 <ishanrana363@gmail.com>
2024-09-15 14:20:37 +0600

    data load and show display

--- milestone-6/Api-JSON/dataload.js
+++ milestone-6/Api-JSON/dataload.js
@@ -1,5 +1,25 @@
 function dataLoad () {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then((res)=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>dataOtherFun(data))
 };
+
+function dataOtherFun (data) {
+    console.log(data)
+}
+
+
+function dataList () {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((data)=>otherData(data))
+}
+
+function otherData (data) {
+    for(let item of data){
+        let ulList = document.getElementById("ul-data");
+        let li = document.createElement("li");
+        li.innerText = item.name ;
+        ulList.appendChild(li)
+    }
+}
