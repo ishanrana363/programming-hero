b447b2beb02b393849f2248572bee2cf27e804de
ishanrana363 <ishanrana363@gmail.com>
2024-09-15 12:35:25 +0600

    json

---
+++ milestone-6/Api-JSON/json.js
@@ -0,0 +1,13 @@
+const man = {
+    name : "kodu",
+    height : 6,
+    age : 32,
+    dis : "dinajpur",
+    isHonest : true
+};
+
+console.log(typeof man)
+
+const parseValuetoJson = JSON.stringify(man);
+
+console.log( typeof parseValuetoJson);
