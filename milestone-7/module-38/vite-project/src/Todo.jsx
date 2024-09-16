6b3ada063cc827a380864e0e649783d16e9f1220
ishanrana363 <ishanrana363@gmail.com>
2024-09-16 22:46:38 +0600

    condictional reandering method 2 done

--- milestone-7/module-38/vite-project/src/Todo.jsx
+++ milestone-7/module-38/vite-project/src/Todo.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
+// import React from 'react';
 
-const Todo = ({name,isDone}) => {
-    if(isDone){
-        return (
-            <div>
-                <ul>
-                    <li> Todo finish :  {name} </li>
-                </ul>
-            </div>
-        );
-    }
-    return (
-        <div>
-            <li>Learnig todo : {name}</li>
-        </div>
-    )
-};
+// const Todo = ({name,isDone}) => {
+//     if(isDone){
+//         return (
+//             <div>
+//                 <ul>
+//                     <li> Todo finish :  {name} </li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+//     return (
+//         <div>
+//             <li>Learnig todo : {name}</li>
+//         </div>
+//     )
+// };
 
-export default Todo;
+// export default Todo;
+
+// const Todo = ({name,isDone}) => {
+//   return (
+//     <div>
+//       <li> {name} : {isDone ? `todo finish` : `learning` } </li>
+//     </div>
+//   )
+// }
+
+// export default Todo
