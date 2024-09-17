74c461de7a45f4438f457500705abb9325027caa
ishanrana363 <ishanrana363@gmail.com>
2024-09-17 13:41:09 +0600

    every thing update

---
+++ milestone-6/intro-es6/objectkey.js
@@ -0,0 +1,13 @@
+const person = {
+    name : "ishan",
+    roll : 500510,
+    height : "6 inch",
+    age : 21
+};
+
+const keysValue = Object.keys(person);
+const objectValues = Object.values(person)
+
+console.log(keysValue);
+
+console.log(objectValues);
